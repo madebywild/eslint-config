6b24795d7424c642cf34364de11b8b78c56e3d2e
Oskar Rainer <rainer.oskar@gmail.com>
2020-11-23 10:20:17 +0100

    feat(import): add basic import rules

--- index.js
+++ index.js
@@ -29,5 +29,7 @@ module.exports = {
     "jsx-a11y/no-noninteractive-element-interactions": "warn",
     "jsx-a11y/click-events-have-key-events": "warn",
     "jsx-a11y/no-static-element-interactions": "warn",
+    "import/order": "error",
+    "import/no-default-export": "warn",
   },
 };
