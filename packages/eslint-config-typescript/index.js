161a990a67f655b5b89e26095599d29aa3e882bb
Stefan Kernjak <stefan@wild.as>
2021-08-04 17:20:34 +0200

    eslint-config-typescript, moved typescript rules back into overrides

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -1,15 +1,20 @@
 module.exports = {
   extends: ["@madebywild/eslint-config-base"],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-empty-function": "warn",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        ignoreRestSiblings: true,
-        args: "none",
-        varsIgnorePattern: "^_",
+  overrides: [
+    {
+      files: ["**/*.ts?(x)"],
+      rules: {
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/no-empty-function": "warn",
+        "@typescript-eslint/no-unused-vars": [
+          "warn",
+          {
+            ignoreRestSiblings: true,
+            args: "none",
+            varsIgnorePattern: "^_",
+          },
+        ],
       },
-    ],
-  },
+    },
+  ],
 };
