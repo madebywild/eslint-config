409e4584f29af4c1d8c7c25350d936320dd95b0b
Stefan Kernjak <stefan@wild.as>
2021-08-04 14:50:39 +0200

    split up eslint config, added lerna, bumped version to 0.2.3

---
+++ packages/eslint-config-base/index.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: ["plugin:prettier/recommended"],
+  plugins: ["prettier"],
+  overrides: [],
+  rules: {
+    "no-var": "error",
+    "prefer-const": "error",
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "import/no-default-export": "warn",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        pathGroups: [
+          {
+            pattern: "~/**",
+            group: "internal",
+          },
+        ],
+      },
+    ],
+  },
+};
---
+++ packages/eslint-config/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ["react-app", "@madebywild/eslint-config-typescript"],
+  overrides: [],
+  rules: {
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/no-unescaped-entities": "off",
+    "react/self-closing-comp": "error",
+    "jsx-a11y/anchor-is-valid": "warn",
+    "jsx-a11y/no-noninteractive-element-interactions": "warn",
+    "jsx-a11y/click-events-have-key-events": "warn",
+    "jsx-a11y/no-static-element-interactions": "warn",
+  },
+};
