e2593333e48517bb7c2307ebc397b27c490ac9da
Lorenzo Migliorero <lorenzo@wild.as>
2022-06-20 12:56:10 +0200

    feat: upgrade ts parser deps

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -4,6 +4,7 @@ module.exports = {
     {
       files: ["**/*.ts?(x)"],
       parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
       rules: {
         "@typescript-eslint/no-explicit-any": "warn",
         "@typescript-eslint/no-empty-function": "warn",
