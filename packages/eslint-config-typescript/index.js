d6a5a1aa8b4e3e3abc3ef81750d4b7fb11e7928c
Oskar Rainer <rainer.oskar@gmail.com>
2022-09-21 09:37:21 +0200

    feat: disable no-undef rule for ts file

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -6,6 +6,7 @@ module.exports = {
       parser: "@typescript-eslint/parser",
       plugins: ["@typescript-eslint"],
       rules: {
+        "no-undef": "off",
         "@typescript-eslint/no-explicit-any": "warn",
         "@typescript-eslint/no-empty-function": "warn",
         "@typescript-eslint/no-unused-vars": [
